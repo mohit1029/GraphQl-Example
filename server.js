168a2e2729950e4cac4c01692ca08b02039ce524
Mohit Chhajer <mohit.chhajer@hashouttech.com>
2022-06-15 14:51:05 +0530

    GraphQl-Example

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+
+const {ApolloServer} = require('apollo-server-express');
+const {loadFilesSync} = require('@graphql-tools/load-files');
+const {makeExecutableSchema} = require('@graphql-tools/schema');
+
+const typesArray = loadFilesSync(path.join(__dirname,'**/*.graphql'));
+const resolverArray = loadFilesSync(path.join(__dirname,'**/*.resolver.js'));
+
+
+async function startApolloServer(){
+    const app = express();
+
+    const schema = makeExecutableSchema({
+        typeDefs: typesArray,
+        resolvers: resolverArray
+    });
+
+   const server =  new ApolloServer({
+    schema
+
+   });
+
+   await server.start();
+   server.applyMiddleware({app,path: '/graphql'});
+   
+    app.listen(3000, () =>{
+        console.log('Running GrapgQl server...');
+    });
+}
+
+startApolloServer();
